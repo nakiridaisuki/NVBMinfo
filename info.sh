962208c1d2db87e2bf1165fb8b1ef985efbfd079
nakiri5500 <nakiridaisuki5500@gmail.com>
2025-11-22 22:28:12 +0800

    fix

--- info.sh
+++ info.sh
@@ -3,9 +3,11 @@ set -e
 SIF_DIR=$HOME/sifs
 sifs=$(ls -1 $SIF_DIR)
 
+./get_tree.sh
+
 for sif in $sifs; do
     file_id=${sif:15:-4}
-    ./get_tree.sh
     singularity run $SIF_DIR/$sif ~/NVBMinfo/get_exec.sh $file_id
 done
+
 singularity run $SIF_DIR/${sifs[-1]} ~/NVBMinfo/extract.sh
