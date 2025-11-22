05982ea6691d1c4a9261808e3025713ede1abf42
nakiri5500 <nakiridaisuki5500@gmail.com>
2025-11-22 15:51:28 +0800

    fix tree

--- tree.sh
+++ tree.sh
@@ -1,3 +1,4 @@
+set -e
 SIF_DIR=$HOME/sifs
 INFO_DIR=./trees
 WORKSPACE_DIR=$HOME/workspaces
@@ -6,5 +7,8 @@ sifs=$(ls -1 $SIF_DIR)
 
 for sif in $sifs; do
     file_id=${sif:15:-4}
-    singularity run $sif cp /workspace/* $WORKSPACE_DIR/$file_id
+    mkdir -p $WORKSPACE_DIR/$file_id
+    mkdir -p $INFO_DIR
+    singularity run $SIF_DIR/$sif cp -r /workspace $WORKSPACE_DIR/$file_id
     tree $WORKSPACE_DIR/$file_id > $INFO_DIR/$file_id
+done
