10d1409737de4cba05122ffc8aa1376d09200c97
nakiri5500 <nakiridaisuki5500@gmail.com>
2025-11-22 22:24:30 +0800

    fix something

--- get_exec.sh
+++ get_exec.sh
@@ -4,13 +4,14 @@ set -e
 EXEC_PATH=$HOME/execs/$1
 EXEC_TYPES=(xhpl xhpcg xhpl_mxp)
 
-if ! [[ -e $EXEC_PATH ]];
+if ! [[ -e $EXEC_PATH ]]; then
+    echo "mkdir -p $EXEC_PATH"
     mkdir -p $EXEC_PATH
 fi
 
 for type in $EXEC_TYPES; do
     path=$(find /workspace -name $type)
-    if ! [[ -z $path ]];
+    if ! [[ -z $path ]]; then
         cp $path $EXEC_PATH/$type
     fi
 done
